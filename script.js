5a7e62d14f140a586b7d4214c27da7bb6fe91eb4
redouane-CEF <redouanemoujahid7@gmail.com>
2025-01-06 11:44:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ console.log("Scipt chargé!");
 const myForm = document.querySelector("form");
 console.log("myForm contient :", myForm);
 myForm.addEventListener("submit", stopReload);
-function stopReload(event) {
-    event.preventDefault();
-    console.log("Formulaire envoyé!");
+function stopReload(evenement) {
+    evenement.preventDefault();
     }
